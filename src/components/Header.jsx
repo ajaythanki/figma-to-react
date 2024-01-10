3e2b7c5019623d0c79fe20fa1f1e13a9c989290c
Ajay Thanki <akthanki5@gmail.com>
2024-01-10 20:02:06 +0530

    Update Header.jsx
    created header navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,34 +2,72 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../assets/logo.png'
 import searchIcon from '../assets/search-icon.svg'
-import { Button, Form, Image, Nav } from 'react-bootstrap';
+import UserCard from '../components/UserCard'
+import profileImage1 from "../assets/profile1.jpg";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from '../redux/features/userSlice';
+import { Button, Form, Image, Nav, NavDropdown } from 'react-bootstrap';
 
-const Header=()=> {
+const Header=({signUpModalShow, setSignupModalShow, signinModalShow,setSigninModalShow})=> {
+
+  const handleShowModal=(modal)=>{
+    if (modal === "signup") {
+      setSignupModalShow(true);
+      setSigninModalShow(false);
+    } else {
+      setSigninModalShow(true);
+      setSignupModalShow(false);
+    }
+  }
+  const user = useSelector(state=>state.user.currentUser);
+  const dispatch = useDispatch()
+  console.log(user);
   return (
-    <Navbar fixed="top" expand="lg" bg='white'>
-      <Container>
-      <Navbar.Brand href="#home">
-        <Image src={logo} alt="logo" width={120} />
-      </Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse id="navbarSupportedContent">
-        <Form class="d-flex mx-auto my-2 px-3 rounded-pill border bg-light border-2 w-25">
-          <Image alt="search" src={searchIcon} className='mr-n5' />
-          <Form.Control
-            type="search"
-            placeholder="Search"
-            className="me-2 px-3 rounded-pill border-0 shadow-none bg-transparent"
-            aria-label="Search"
-          />
-        </Form>
-        <Nav>
-          <Nav.Item>
-            <Navbar.Text className='' >
-              Create account. <a href="#signup">It’s free!</a>
+    <Navbar expand="lg" fixed='top' bg='white'>
+      <Container fluid>
+        <Navbar.Brand href="/"><Image src={logo}/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Form className="d-flex me-auto ms-auto rounded-pill bg-body-tertiary searchform">
+            <Button variant="btn">
+              <Image src={searchIcon} />
+            </Button>
+            <Form.Control
+              type="search"
+              name="search"
+              placeholder="Search for your favorite groups in ATG"
+              className="me-2 border-0 bg-transparent shadow-none"
+              style={{ width: 360 }}
+              aria-label="Search"
+            />
+          </Form>
+          <Nav
+            className="my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            {user &&
+            <>
+              <Navbar.Text className="d-flex align-items-center">
+                <UserCard profileImage={profileImage1} />
+                <NavDropdown title={user.firstName +' '+ user.lastName} id="navbarScrollingDropdown">
+                  <NavDropdown.Item onClick={()=>dispatch(logout())}>Log out</NavDropdown.Item>
+                </NavDropdown>
+              </Navbar.Text>
+            </>
+
+            }
+            {!user &&
+            <Navbar.Text className="d-flex align-items-center">
+              Create account.
+              <NavDropdown title="It’s free" id="navbarScrollingDropdown">
+                <NavDropdown.Item onClick={()=>handleShowModal('signup')}>Signup</NavDropdown.Item>
+                <NavDropdown.Item onClick={()=>handleShowModal('signin')}>Login</NavDropdown.Item>
+              </NavDropdown>
             </Navbar.Text>
-          </Nav.Item>
-        </Nav>
-      </Navbar.Collapse>
+            }
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
