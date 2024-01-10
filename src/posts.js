cd5f374ac78fdc5554d64ce69416c21e0850e5f2
Ajay Thanki <akthanki5@gmail.com>
2024-01-10 22:54:25 +0530

    Create posts.js

---
+++ src/posts.js
@@ -0,0 +1,57 @@
+import PostImage1 from "./assets/blog-1.jpg";
+import PostImage2 from "./assets/blog-2.jpg";
+import PostImage3 from "./assets/blog-3.jpg";
+import profileImage1 from "./assets/profile1.jpg";
+import profileImage2 from "./assets/profile2.jpg";
+import profileImage3 from "./assets/profile3.jpg";
+import profileImage4 from "./assets/profile4.jpg";
+
+export const posts = [
+  {
+    id: "1",
+    category: "article",
+    title: "What if famous brands had regular fonts? Meet RegulaBrands!",
+    subtitle:
+      "I’ve worked in UX for the better part of a decade. From now on, I plan to rei…",
+    imgURL: PostImage1,
+    authorName: "Sarthak Kamra",
+    authorImgURL: profileImage1,
+  },
+  {
+    id: "2",
+    category: "education",
+    title:
+      "Tax Benefits for Investment under National Pension Scheme launched by Government",
+    subtitle:
+      "I’ve worked in UX for the better part of a decade. From now on, I plan to rei…",
+    imgURL: PostImage2,
+    authorName: "Sarah West",
+    authorImgURL: profileImage2,
+  },
+  {
+    id: "3",
+    category: "meetup",
+    title: "Finance & Investment Elite Social Mixer @Lujiazui",
+    subtitle:
+      "I’ve worked in UX for the better part of a decade. From now on, I plan to rei…",
+    imgURL: PostImage3,
+    authorName: "Ronal Jones",
+    authorImgURL: profileImage3,
+    date: "Fri, 12 Oct, 2018",
+    location: "Ahmedabad, India",
+    buttonLabel: "Visit Website",
+    buttonURL: "#",
+  },
+  {
+    id: "4",
+    category: "job",
+    title: "Software Developer",
+    companyName: "Innovaccer Analytics Private Ltd.",
+    imgURL: PostImage2,
+    authorName: "Joseph Gray",
+    authorImgURL: profileImage4,
+    location: "Noida, India",
+    buttonLabel: "Apply on Timesjobs",
+    buttonURL: "#",
+  },
+];
